2f54ab548265ae3c8a13b635f4c8165decb6e747
Ale <alex1313shi@gmail.com>
2025-01-28 21:15:57 -0600

    prueba2

--- src/app/dashboard/pages/defer-options/defer-options.component.ts
+++ src/app/dashboard/pages/defer-options/defer-options.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CardComponent } from '../../../card/card/card.component';
 
 @Component({
   selector: 'app-defer-options',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './defer-options.component.html',
   styles: `
     :host {
