5110632eb887a1611b7b9a4aefc3088ffa899d01
Ale <alex1313shi@gmail.com>
2025-01-28 22:02:38 -0600

    prueba2

--- src/app/dashboard/shared/menu/menu.component.ts
+++ src/app/dashboard/shared/menu/menu.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterLink, RouterOutlet, Routes } from '@angular/router';
+export const routes: Routes= [];
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  constructor(private router: Router) {}
 
+  redirectToHome() {
+    this.router.navigate(['/dashboard/defer-options']);
+  }
+  
 }
